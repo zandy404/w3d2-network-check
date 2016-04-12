4ffad6b8de4bc26e5cdbf64f168c640264a2cf9c
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-12 13:25:04 -0400

    Optimize imports

--- app/src/main/java/com/example/networkingstatus/MainActivity.java
+++ app/src/main/java/com/example/networkingstatus/MainActivity.java
@@ -3,11 +3,10 @@ package com.example.networkingstatus;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
