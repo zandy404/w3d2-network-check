c45394a56357a6090ea32b666a01696236d1268f
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-12 13:24:37 -0400

    Add checkInternet logic

--- app/src/main/java/com/example/networkingstatus/MainActivity.java
+++ app/src/main/java/com/example/networkingstatus/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.networkingstatus;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +20,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void checkInternet(View view) {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
 
+        if (activeNetwork != null) {
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) {
+                Log.d(TAG, "checkInternet: " + "Connected to WIFI");
+            } else if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE) {
+                Log.d(TAG, "checkInternet: " + "Connected to Mobile data");
+            }
+        } else {
+            Log.d(TAG, "checkInternet: " + "Not connected");
+        }
     }
 }
